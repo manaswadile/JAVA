a9bd7f747f8e55c7ce2f718d96e1828146843711
manaswadile <wadilemanas2006@gmail.com>
2025-08-10 22:00:44 +0530

    Basics Done

--- Hello.java
+++ Hello.java
@@ -1,6 +1,56 @@
 class Hello{
     public static void main(String a[]){
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+
+        // System.out.println(3 + 6);
+
+        // int num1 = 3;
+        // int num2 = 9;
+        // int result = num1 + num2;
+        // System.out.println(result);
+
+        //###################################################################
+        
+        
+        // int num = 5;       // 4 byte
+        // byte by = 127;     // 1 byte
+        // short sh = 456;    // 2 byte
+        // long l = 93797l;   // 8 byte //for long you need to add L at last
+
+        // float f = 3.6f;   // 4 byte //for float f is to be added at last
+        // double d = 3.6;   // 8 byte //default 
+
+        // char c = 'k';     // 2 byte //you only need single quotes ''
+
+        // boolean b = true;   //No (0,1) only true or false
+        // boolean b = 1 or 0; //it will give error
+        
+
+
+        //##################################################################
+
+
+        //literals
+
+        // int num1 = 0b101;  
+        // int num2 = 0x7E;
+        // System.out.println(num1);
+        // System.out.println(num2);
+
+        // int num = 100_00_000;   //to differentiate we can use _ if the no. of zeros are more
+        // System.out.println(num);
+
+        // double n = 10e4;   //it is 10^4 
+
+
+        char c = 'a';
+        c++;  // it will print next char according to order
+        System.out.println(c);
+
+
+
+
+
     }   
 }
